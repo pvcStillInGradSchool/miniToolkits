77eba681eb7f722de4d1b8e6d699179f2353b4f9
Weicheng Pei <weicheng.pei@icloud.com>
2019-03-26 16:50:06 +0800

    Define Scheduler's interface.

---
+++ scheduler.py
@@ -0,0 +1,26 @@
+"""Define Scheduler."""
+
+class Scheduler:
+    """Schedule a set of tasks."""
+
+    def __init__(self):
+        pass
+
+    def add_task(self, task):
+        """Add a new task."""
+
+    def count_tasks(self):
+        """Return the number of tasks being added."""
+
+    def add_dependency(self, task, prerequisite):
+        """Add a new dependency."""
+
+    def count_dependencies(self):
+        """Return the number of dependencies being added."""
+
+    def schedule(self):
+        """Schedule added tasks being added."""
+
+
+if __name__ == "__main__":
+    pass
---
+++ scheduler_test.py
@@ -0,0 +1,25 @@
+"""Test Scheduler."""
+
+import unittest
+
+from scheduler import Scheduler
+
+
+class TestScheduler(unittest.TestCase):
+    """Test Scheduler."""
+
+    def test_add_and_count_tasks(self):
+        """Test add_tasks() and count_tasks()."""
+        self.fail()
+
+    def test_add_and_count_dependency(self):
+        """Test add_dependency() and count_dependencies()."""
+        self.fail()
+
+    def test_schedule(self):
+        """Test schedule()."""
+        self.fail()
+
+
+if __name__ == "__main__":
+    unittest.main()
