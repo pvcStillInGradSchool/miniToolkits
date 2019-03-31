2fc30763b2efee23a0b2d35c9a54c55c36448022
Weicheng Pei <weicheng.pei@icloud.com>
2019-03-31 11:10:01 +0800

    Rewrite tests for Scheduler.

--- scheduler_test.py
+++ scheduler_test.py
@@ -6,10 +6,17 @@ from scheduler import Scheduler
 
 
 class TestScheduler(unittest.TestCase):
-    """Test Scheduler."""
+    """Test the correctness of scheduler.Scheduler."""
 
-    def test_add_and_count_tasks(self):
-        """Test add_a_task(), add_tasks() and n_tasks()."""
+    def test_empty_task_set(self):
+        """Test public methods on an empty task set."""
+        a_scheduler = Scheduler()
+        self.assertEqual(a_scheduler.n_tasks(), 0)
+        schedued_tasks = a_scheduler.schedule()
+        self.assertEqual(schedued_tasks, set())
+
+    def test_linked_list(self):
+        """Test public methods on a task set, which forms a linked list."""
         a_scheduler = Scheduler()
         self.assertEqual(a_scheduler.n_tasks(), 0)
         a_scheduler.add_a_task('A')
@@ -20,57 +27,75 @@ class TestScheduler(unittest.TestCase):
         self.assertEqual(a_scheduler.n_tasks(), 2)
         a_scheduler.add_tasks(('C', 'D'))
         self.assertEqual(a_scheduler.n_tasks(), 4)
-
-    def test_add_and_check_dependency(self):
-        """Test add_a_prerequisite(), add_prerequisites() and check_dependency()."""
-        a_scheduler = Scheduler()
-        # No dependency in an empty graph.
-        self.assertEqual(a_scheduler.n_tasks(), 0)
-        self.assertFalse(a_scheduler.check_dependency('B', 'A'))
-        self.assertFalse(a_scheduler.check_dependency('A', 'B'))
-        # A unidirectional dependency between two tasks: A <- B.
+        # Build the dependency graph, which is a linked list:
+        #   A <- B <- C <- D
         a_scheduler.add_a_prerequisite(task='B', prerequisite='A')
-        self.assertEqual(a_scheduler.n_tasks(), 2)
-        self.assertTrue(a_scheduler.check_dependency('B', 'A'))
-        self.assertFalse(a_scheduler.check_dependency('A', 'B'))
-        # Make a dependency list: A <- B <- C.
         a_scheduler.add_a_prerequisite(task='C', prerequisite='B')
-        self.assertEqual(a_scheduler.n_tasks(), 3)
-        self.assertTrue(a_scheduler.check_dependency('C', 'B'))
-        self.assertTrue(a_scheduler.check_dependency('B', 'A'))
-        # C indirectly depends on A.
-        self.assertTrue(a_scheduler.check_dependency('C', 'A'))
-        # No dependency in the opposite direction.
-        self.assertFalse(a_scheduler.check_dependency('A', 'B'))
-        self.assertFalse(a_scheduler.check_dependency('B', 'C'))
-        self.assertFalse(a_scheduler.check_dependency('A', 'C'))
-        # Add multiple prerequisites.
+        a_scheduler.add_a_prerequisite(task='D', prerequisite='C')
+        # Only one result is correct.
+        schedued_tasks = a_scheduler.schedule()
+        self.assertEqual(schedued_tasks, {('A', 'B', 'C', 'D')})
+
+    def test_binary_tree(self):
+        """Test public methods on a task set, which forms a binary tree."""
+        a_scheduler = Scheduler()
+        # Build the dependency graph, which is a binary tree:
+        #     A
+        #    / \
+        #   B   C
+        a_scheduler.add_a_prerequisite(task='B', prerequisite='A')
+        a_scheduler.add_a_prerequisite(task='C', prerequisite='A')
+        # Two possible results:
+        #   - A must appears before B and C.
+        #   - No requirements on the relative order of B and C.
+        schedued_tasks = a_scheduler.schedule()
+        self.assertTrue(schedued_tasks in (
+            {('A', 'B', 'C')},
+            {('A', 'C', 'B')}
+        ))
+
+    def test_reversed_binary_tree(self):
+        """Test public methods on a task set, which forms a reversed binary tree."""
+        a_scheduler = Scheduler()
+        # Build the dependency graph, which is a reversed binary tree:
+        #   B   C
+        #    \ /
+        #     A
         a_scheduler.add_prerequisites(task='A', prerequisites=('B', 'C'))
-        self.assertEqual(a_scheduler.n_tasks(), 3)
-        self.assertTrue(a_scheduler.check_dependency('A', 'B'))
-        self.assertTrue(a_scheduler.check_dependency('B', 'C'))
-        self.assertTrue(a_scheduler.check_dependency('A', 'C'))
+        # Two possible results:
+        #   - A must appears after B and C.
+        #   - No requirements on the relative order of B and C.
+        schedued_tasks = a_scheduler.schedule()
+        self.assertTrue(schedued_tasks in (
+            {('B', 'C', 'A')},
+            {('C', 'B', 'A')}
+        ))
 
-    def test_schedule(self):
-        """Test schedule()."""
+    def test_multiple_linked_lists(self):
+        """Test public methods on a task set, which forms two linked lists."""
         a_scheduler = Scheduler()
+        # Build the first linked list:
+        #   A <- B <- C
         a_scheduler.add_a_prerequisite(task='B', prerequisite='A')
         a_scheduler.add_a_prerequisite(task='C', prerequisite='B')
-        scheduled_tasks = a_scheduler.schedule()
-        self.assertEqual(scheduled_tasks, {('A', 'B', 'C')})
-        # Make a dependency graph with multiple independent components.
+        # Build the second linked list:
+        #   1 <- 2 <- 3
         a_scheduler.add_a_prerequisite(task=2, prerequisite=1)
-        a_scheduler.add_prerequisites(task=3, prerequisites=(2, 1))
+        a_scheduler.add_a_prerequisite(task=3, prerequisite=2)
+        # Only one result is correct.
         scheduled_tasks = a_scheduler.schedule()
         self.assertEqual(scheduled_tasks, {('A', 'B', 'C'), (1, 2, 3)})
-        a_scheduler.add_a_prerequisite(task=2, prerequisite=4)
-        scheduled_tasks = a_scheduler.schedule()
-        self.assertTrue(scheduled_tasks in ({('A', 'B', 'C'), (1, 4, 2, 3)},
-                                            {('A', 'B', 'C'), (4, 1, 2, 3)}))
-        # Make a cycle in the dependency graph.
-        a_scheduler.add_a_prerequisite(task='A', prerequisite='C')
+
+    def test_cycle_detection(self):
+        """Test public methods on a task set, which forms a cycle."""
+        a_scheduler = Scheduler()
+        # Build the dependency graph, which is a cycle:
+        #   A -> B -> C -> A
+        a_scheduler.add_a_prerequisite(task='A', prerequisite='B')
+        a_scheduler.add_a_prerequisite(task='B', prerequisite='C')
+        a_scheduler.add_a_prerequisite(task='C', prerequisite='A')
         with self.assertRaises(AssertionError):
-            scheduled_tasks = a_scheduler.schedule()
+            a_scheduler.schedule()
 
 
 if __name__ == "__main__":
