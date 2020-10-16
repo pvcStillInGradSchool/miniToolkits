eff169c6852abd47a25dc720f9fc252baf25b80e
Weicheng Pei <weicheng.pei@icloud.com>
2020-10-16 12:39:39 +0800

    Clarify code and message.

--- scan_and_replace.py
+++ scan_and_replace.py
@@ -8,13 +8,6 @@ import pathlib
 import sys
 
 
-def get_absolute_path(posix_path):
-  name = str()
-  for i in range(1, len(posix_path.parts)):
-    name += '/' + posix_path.parts[i]
-  return name
-
-
 def scan_and_print(sys_argv):
   root = pathlib.Path(sys_argv[1])
   suffix = sys_argv[2]
@@ -23,7 +16,7 @@ def scan_and_print(sys_argv):
   paths = list(root.glob('**/*.' + suffix))
   total_count = 0
   for path in paths:
-    old_name = get_absolute_path(path)
+    old_name = str(path)
     count = 0
     with path.open() as old_file:
       for line in old_file:
@@ -44,7 +37,7 @@ def scan_and_replace(sys_argv):
   new_str = sys_argv[4]
   paths = list(root.glob('**/*.' + suffix))
   for path in paths:
-    old_name = get_absolute_path(path)
+    old_name = str(path)
     new_name = old_name + '.temp'
     with path.open() as old_file:
       with open(new_name, 'w') as new_file:
@@ -54,11 +47,12 @@ def scan_and_replace(sys_argv):
 
 
 if __name__ == '__main__':
-  if(len(sys.argv) != 5):
+  if (len(sys.argv) != 5):
     print('Usage:')
     print('  python3 scan_and_replace.py [root] [suffix] [old] [new]')
   else:
     if scan_and_print(sys.argv) > 0:
-      print('Are you sure to replace these words?[y/n]', end='', flush=True)
+      print('Are you sure to replace these "{0}"s to "{1}"s? [y/n]'.format(
+          sys.argv[3], sys.argv[4]), end='', flush=True)
       if (sys.stdin.read(1) == 'y'):
         scan_and_replace(sys.argv)
